318aab34a132b3618d603e807023105c60662fd9
dpazdzior21 <dpazdzior@protonmail.com>
2025-04-08 17:34:15 +0200

    Add Put method for PositionController

--- src/main/java/pl/polsl/FootballLeague/controller/PositionController.java
+++ src/main/java/pl/polsl/FootballLeague/controller/PositionController.java
@@ -1,9 +1,14 @@
 package pl.polsl.FootballLeague.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,9 +26,24 @@ public class PositionController {
 	public @ResponseBody void addPosition(@RequestBody Position position) {
 		positionRepo.save(position);
 	}
-	
+
 	@GetMapping
-	public @ResponseBody Iterable<Position> getPositions(){
+	public @ResponseBody Iterable<Position> getPositions() {
 		return positionRepo.findAll();
 	}
+
+	@PutMapping("/{id}")
+	public @ResponseBody ResponseEntity<?> updatePosition(@PathVariable Integer id,
+			@RequestBody Position updatedPosition) {
+		Optional<Position> optionalPosition = positionRepo.findById(id);
+
+		if (optionalPosition.isPresent()) {
+			Position existingPosition = optionalPosition.get();
+			existingPosition.setName(updatedPosition.getName());
+			positionRepo.save(existingPosition);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+
 }
